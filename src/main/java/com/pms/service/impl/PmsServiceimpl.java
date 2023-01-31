c55d29bb5d6fe53044b45f5eb6125500181f3206
JeoungHyun <cho3153238@naver.com>
2023-01-31 15:31:38 +0900

    20230131
    20230131

---
+++ src/main/java/com/pms/service/impl/PmsServiceimpl.java
@@ -0,0 +1,33 @@
+package com.pms.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.pms.service.PmsService;
+import com.pms.service.PmsVO;
+
+@Service("PmsService")
+public class PmsServiceimpl implements PmsService{
+
+	@Resource(name = "PmsMapper")
+	private PmsMapper PmsMapper;
+	
+	@Override
+	public void createProject(PmsVO pmsvo) {
+		PmsMapper.createProject(pmsvo);
+	}
+
+	@Override
+	public List<PmsVO> selectProjectList() {
+		return PmsMapper.selectProjectList();
+	}
+
+	@Override
+	public PmsVO selectProject(PmsVO pmsvo) {
+		return PmsMapper.selectProject(pmsvo);
+	}
+
+}
